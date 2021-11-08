116726b7143fe0c646275b92f2a9f20e407f046f
a-buroff <a-buroff@yandex.ru>
2021-11-08 17:55:27 +0300

    Anonymus user problem fixed, /me endpoint still not working

--- api_yamdb/users/permissions.py
+++ api_yamdb/users/permissions.py
@@ -8,12 +8,13 @@ class AdminOrOwnProfile(permissions.BasePermission):
             own_data_flag = request.resolver_match.kwargs['username'] == 'me'
         else:
             own_data_flag = False
-
+        if not request.user.is_authenticated:
+            return False
         return (
                 request.user.is_staff
                 or request.user.is_superuser
                 or request.user.role == 'admin'
-                or owner_data_flag
+                or own_data_flag
         )
 
     def has_object_permission(self, request, view, obj):
